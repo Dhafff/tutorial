c84a802e5c91aca67876aed001ca4ec778f4e832
Dhafff <dhafgoes@student.telkomuniversity.ac.id>
2024-02-27 15:18:19 +0700

    nyoba aja

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a, b;
+
+Console.Write("Masukan nama anda: ");
+a = Console.ReadLine ();
+
+Console.WriteLine("Hai, " + a);
+/*
+if (a + b == 3)
+{
+    Console.Write(true);
+} else
+{
+    Console.Write(false);
+}
+*/
